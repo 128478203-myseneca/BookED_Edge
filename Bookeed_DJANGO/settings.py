3ccb77f3ee65f748218cf9e876db875c1290131d
indiafoxtrot <istvanf8@hotmail.com>
2020-10-30 17:19:39 -0400

    fixed password reset, few other details

--- Bookeed_DJANGO/settings.py
+++ Bookeed_DJANGO/settings.py
@@ -155,5 +155,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'booked.reset@gmail.com'
-EMAIL_HOST_PASSWORD = 'lnshv0BmVD9xtbgjr0W6'
+EMAIL_HOST_PASSWORD = 'uwppxwumolicojaq'
 
