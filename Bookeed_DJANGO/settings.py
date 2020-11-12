6de2672f1b303b5c7a51046fb3b1a1558d22f38b
unknown <indiafoxtrot>
2020-11-12 15:39:38 -0500

    changed password email, mobile,few other minor changes

--- Bookeed_DJANGO/settings.py
+++ Bookeed_DJANGO/settings.py
@@ -153,4 +153,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "booked.reset@gmail.com"
-EMAIL_HOST_PASSWORD = "uwppxwumolicojaq"
+EMAIL_HOST_PASSWORD = "hdaxgaspnpicmlrl"
