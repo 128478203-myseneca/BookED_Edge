6dab3abf784619ade069587bd218f64c651ebbbf
Istvan Feher <62614194+128478203-myseneca@users.noreply.github.com>
2023-08-16 21:25:11 -0400

    Update settings.py

--- Bookeed_DJANGO/settings.py
+++ Bookeed_DJANGO/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "booked.reset@gmail.com"
+EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
